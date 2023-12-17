b05e9fe32422cccf6845d1805ad831d234ef503e
Bobby Hall <bobbyhall@Bobbys-MacBook-Pro.local>
2023-12-17 12:57:24 -0500

    added landing page to get emails

---
+++ src/pages/prelaunch.tsx
@@ -0,0 +1,12 @@
+import { BackgroundBeams } from "@/components/BackgroundBeams";
+
+const LandingPage: React.FC = () => {
+    return (
+        <div className="">
+            <BackgroundBeams />
+        </div>
+
+)
+}
+
+export default LandingPage;
